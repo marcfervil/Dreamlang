9b9595e29fa26db29698b45f252bcc8f70c2a669
Marc <mfervil1@depaul.edu>
2021-02-03 16:44:24 -0600

    Slight cleanup

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
 from parser import *
 from lexar import *
 
-#print("love is", true, 4, "ever")
+# print("love is", true, 4, "ever")
+
 
 def test(name, func):
     print(name, "Passed" if func() else "Failed")
 
+
 def test_math():
-    testData = '3*11-4+4-99*100/2/5-300-89-345*30'
-    tokens = Tokenizer(testData).tokenize()
+    test_data = '3*11-4+4-99*100/2/5-300-89-345*30'
+    tokens = Tokenizer(test_data).tokenize()
     result = str(Parser(tokens).get_ast())
-    return eval(testData) == eval(result)
+    return eval(test_data) == eval(result)
+
+
+def print_results(test_data):
+    tokens = Tokenizer(test_data).tokenize()
+    result = str(Parser(tokens).get_ast())
+    print(result)
+
 
 test("Math:", test_math)
 
-testData = 'print('
-
-tokens = Tokenizer(testData).tokenize()
-result = str(Parser(tokens).get_ast())
+print_results('print("h", 3*11, "lz")')
 
 
-print(result)
 
 
 
